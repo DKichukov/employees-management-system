fb814e4e3b86bcba579b6400750fe170ef2e7fdf
DKichukov <dzhamal.kichukov@gmail.com>
2023-08-29 18:59:10 +0300

    add file for db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO employees (first_name, last_name, email) VALUES ('John', 'Doe', 'john.doe@example.com');
+
+INSERT INTO employees (first_name, last_name, email) VALUES ('Jane', 'Smith', 'jane.smith@example.com');
+
+INSERT INTO employees (first_name, last_name, email) VALUES ('Michael', 'Johnson', 'michael.johnson@example.com');
+
+INSERT INTO employees (first_name, last_name, email) VALUES ('Emily', 'Williams', 'emily.williams@example.com');
+
+INSERT INTO employees (first_name, last_name, email) VALUES ('David', 'Brown', 'david.brown@example.com');
+
+INSERT INTO employees (first_name, last_name, email) VALUES ('Olivia', 'Jones', 'olivia.jones@example.com');
+
+INSERT INTO employees (first_name, last_name, email) VALUES ('James', 'Miller', 'james.miller@example.com');
+
+INSERT INTO employees (first_name, last_name, email) VALUES ('Sophia', 'Wilson', 'sophia.wilson@example.com');
+
+INSERT INTO employees (first_name, last_name, email) VALUES ('Liam', 'Taylor', 'liam.taylor@example.com');
+
+INSERT INTO employees (first_name, last_name, email) VALUES ('Emma', 'Anderson', 'emma.anderson@example.com');
